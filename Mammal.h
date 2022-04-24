e94b3cc91f6d08771ebf69fa3eea91eb396bf250
jcortado <jcortado@hawaii.edu>
2022-04-23 23:43:08 -1000

    Mammal Class skeleton

--- Mammal.h
+++ Mammal.h
@@ -12,10 +12,37 @@
 #ifndef EE205_LAB_10D_ANIMAL_FARM_3_MAMMAL_H
 #define EE205_LAB_10D_ANIMAL_FARM_3_MAMMAL_H
 
+#include <string>
+#include "Color.h"
+#include "Weight.h"
 
 class Mammal {
 
+public:
+    static const std::string MAMMAL_NAME;
+
+protected:
+    Color color = Color::UNKNOWN_COLOR;
+
+public:
+    Mammal( const Weight::t_weight newMaxWeight ,const std::string& newSpecies ) : Animal( newMaxWeight, MAMMAL_NAME, newSpecies ) {};
+
+    Mammal( const Color            newColor
+            ,const Gender           newGender
+            ,const Weight::t_weight newWeight
+            ,const Weight::t_weight newMaxWeight
+            ,const std::string&     newSpecies
+    ) : Animal( newGender, newWeight, newMaxWeight, MAMMAL_NAME, newSpecies ) {
+        setColor( newColor );
+    };
+
+
+    Color getColor() const noexcept { return color; }
+    void setColor( const Color newColor ) noexcept { color = newColor; }
+
+
+    void dump() const noexcept override;
 };
 
 
-#endif //EE205_LAB_10D_ANIMAL_FARM_3_MAMMAL_H
+
