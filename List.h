425587c17dc435f6f748171df4d4508611c46012
jcortado <jcortado@hawaii.edu>
2022-04-24 08:08:41 -1000

    List class skeleton

--- List.h
+++ List.h
@@ -8,14 +8,28 @@
 /// @author Jordan Cortado <jcortado@hawaii.edu>
 /// @date   23_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
+#pragma once
 
-#ifndef EE205_LAB_10D_ANIMAL_FARM_3_LIST_H
-#define EE205_LAB_10D_ANIMAL_FARM_3_LIST_H
-
+#include "Node.h"
+#include "config.h"
 
 class List {
+protected:
+    Node* head = nullptr;
+    unsigned int count = 0;
 
+public:
+    static Node* get_next( const Node* currentNode );
+
+    bool empty() const noexcept;
+    unsigned int size() const noexcept;
+    bool isIn( Node* aNode ) const;
+    bool isSorted() const noexcept;
+    Node* get_first() const noexcept;
+    void deleteAllNodes() noexcept;
+
+    virtual Node* pop_front() noexcept = 0;
+    virtual void dump() const noexcept = 0;
+    virtual bool validate() const noexcept = 0;
 };
 
-
-#endif //EE205_LAB_10D_ANIMAL_FARM_3_LIST_H
